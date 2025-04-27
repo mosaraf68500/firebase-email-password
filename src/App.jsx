9bb9aee5a7e589372491245f191cbdc129193a25
Md:Mosaraf Hossen <151431976+mosaraf68500@users.noreply.github.com>
2025-04-27 10:17:34 +0600

    index

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <h1>firebase email and password</h1>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from '../Header/Navbar';
+import { Outlet } from 'react-router';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>home</h1>
+        </div>
+    );
+};
+
+export default Home;
